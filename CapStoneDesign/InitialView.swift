9c39b3aa9e96b10aa6676b6de81bff2e1b4ca4b9
wodnd <wodnd0418@naver.com>
2024-04-07 03:58:50 +0900

    design: splash, initial view

---
+++ CapStoneDesign/InitialView.swift
@@ -0,0 +1,32 @@
+//
+//  InitialView.swift
+//  CapStoneDesign
+//
+//  Created by Wodnd on 4/7/24.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        
+        Spacer()
+        
+        Text("App Name")
+            .font(.system(size: 40, weight: .bold))
+        
+        Spacer()
+        
+        VStack{
+            Text("Naver Login")
+            Text("Google Login")
+        }
+        
+        Spacer()
+            .frame(height: 40)
+    }
+}
+
+#Preview {
+    InitialView()
+}
