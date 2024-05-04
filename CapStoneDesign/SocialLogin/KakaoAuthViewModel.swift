a59c2868c8d8482d8964223867a84b47d2e70d93
wodnd <wodnd0418@naver.com>
2024-05-04 20:35:53 +0900

    feat: kakao access token

---
+++ CapStoneDesign/SocialLogin/KakaoAuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  KakaoAuthViewModel.swift
+//  CapStoneDesign
+//
+//  Created by Wodnd on 5/4/24.
+//
+
+import Foundation
---
+++ CapStoneDesign/SocialLogin/MyAppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  MyAppDelegate.swift
+//  CapStoneDesign
+//
+//  Created by Wodnd on 5/4/24.
+//
+
+import Foundation
